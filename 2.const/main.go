c6e51ddc1037eabf391b231f3d5c68ae9f6cb940
vivekkumarpandey031 <vivekkumarpandey031@gmail.com>
2023-08-08 11:03:28 +0530

    second commit

---
+++ 2.const/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+const PI float32 = 3.14
+const SQUARE_OF_PI = PI*PI
+const(
+	DAYS = 7
+	MONTHS = 30
+	YEAR = 365
+	MIN = 60
+	SECONDS = 60
+)
+
+
+func main()  {
+	
+ fmt.Println(PI)
+
+ var num1 rune = 'a'
+ fmt.Println(num1)
+
+
+}
